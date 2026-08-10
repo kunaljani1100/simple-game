efe3328b5a540bbe1f26bf33dc5ba410617d92db
kunaljani1100 <168258319+kunaljani1100@users.noreply.github.com>
2026-08-09 21:04:00 -0700

    Add action that moves the object downwards in maze game by pressing the down arrow key.

--- move_arrow.py
+++ move_arrow.py
@@ -1,5 +1,26 @@
 import pyautogui
+import subprocess
 
-print(pyautogui.position())
-pyautogui.moveTo(500, 300)
-pyautogui.click(100, 100, 2)
+# print(pyautogui.position())
+# pyautogui.moveTo(500, 300)
+# pyautogui.click(100, 100, 2)
+
+script = '''
+    tell application "System Events"
+        repeat with p in application processes
+            try
+                repeat with w in windows of p
+                    if name of w is "Maze Game" then
+                        perform action "AXRaise" of w
+                        set frontmost of p to true
+                        return
+                    end if
+                end repeat
+            end try
+        end repeat
+    end tell
+    '''
+
+subprocess.run(["osascript", "-e", script], check = True)
+while True:
+    pyautogui.press("down")
